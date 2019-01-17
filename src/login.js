8841c2e1e5340bc984fdbbfe570f03ddd717da3b
Mark Schmieg <ms147@hw.ac.uk>
2019-01-17 17:49:01 +0000

    login

--- src/login.js
+++ src/login.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Login extends Component {
+
   render() {
     return (
       <div className="App">
