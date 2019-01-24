7e7b442204c6709e9056431c423fc5635583a1ec
paul-cockburn <paul.s.cockburn@gmail.com>
2019-01-24 09:29:28 +0000

    new

--- src/App.js
+++ src/App.js
@@ -3,46 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 import ButtonExampleButton from './Btn.js'
 import Nav from './Nav.js'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
-        <ButtonExampleButton />
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/nav">Nav</Link>
+              </li>
+              <li>
+                <Link to="/button">Button</Link>
+              </li>
+            </ul>
 
+            <hr />
+
+            <Route path="/nav" component={Nav} />
+            <Route path="/button" component={ButtonExampleButton} />
+
+          </div>
+        </Router>
     </div>
     );
   }
